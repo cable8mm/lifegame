8b565a23c78439f0b53e4713b285bb1b39bda23c
Sam Lee <cable8mm@gmail.com>
2020-04-28 22:18:15 +0900

    score를 cell의 속성으로 넣고, active판단을 본인이 하게 함.

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,18 +1,35 @@
-import GameConfig from '@/GameConfig';
+import GameConfig from "@/GameConfig";
 
 export default class Cell extends Phaser.Geom.Rectangle {
-    public active: boolean;
-    private scene: Phaser.Scene;
+  public active: boolean;
+  private _score: integer = 0;
+  private scene: Phaser.Scene;
 
-    constructor(scene: Phaser.Scene, position: TwoDimensionPosition, active: boolean) {
-        super(
-            position.x * GameConfig.CELL_SIZE, 
-            position.y * GameConfig.CELL_SIZE, 
-            GameConfig.CELL_SIZE - 1, 
-            GameConfig.CELL_SIZE - 1
-        );
+  constructor(
+    scene: Phaser.Scene,
+    position: TwoDimensionPosition,
+    active: boolean
+  ) {
+    super(
+      position.x * GameConfig.CELL_SIZE,
+      position.y * GameConfig.CELL_SIZE,
+      GameConfig.CELL_SIZE - 1,
+      GameConfig.CELL_SIZE - 1
+    );
 
-        this.scene = scene;
-        this.active = active;
+    this.scene = scene;
+    this.active = active;
+  }
+  get score(): integer {
+    return this._score;
+  }
+  set score(value: integer) {
+    if (this.active && (2 > value || 3 < value)) {
+      this.active = false;
+    } else if (!this.active && 3 == value) {
+      this.active = true;
     }
+
+    this._score = value;
+  }
 }
